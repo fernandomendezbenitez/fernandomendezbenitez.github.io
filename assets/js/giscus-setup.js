68db1bec939e1d9b61f967896a8f55fa088aaf92
fernandomendezbenitez <fernando.mendez.benitez@gmail.com>
2026-05-25 22:52:08 +0000

    Deploying to gh-pages from @ fernandomendezbenitez/fernandomendezbenitez.github.io@c539763cd77b28e5a5e14a27863a32d7d2c451b0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fernandomendezbenitez/fernandomendez.github.io",
+    "data-repo": "fernandomendezbenitez/fernandomendezbenitez.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
