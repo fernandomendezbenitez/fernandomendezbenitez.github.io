ba9c9e736af181a6f33dccaafe35ce1befb15fdd
fernandomendezbenitez <fernando.mendez.benitez@gmail.com>
2026-05-26 14:32:59 +0000

    Deploying to gh-pages from @ fernandomendezbenitez/fernandomendezbenitez.github.io@9edd422d3eacd18198b6114a307d4d16785c1c43 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-vitae",
+          title: "vitae",
           description: "",
           section: "Navigation",
           handler: () => {
