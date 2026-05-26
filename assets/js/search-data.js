6293b7fb2b27552ed165bd71aab9db2fe2db8b18
fernandomendezbenitez <fernando.mendez.benitez@gmail.com>
2026-05-26 14:01:51 +0000

    Deploying to gh-pages from @ fernandomendezbenitez/fernandomendezbenitez.github.io@7eaf974cda077be5f810bae5f6e1520d5bedee51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-current-research",
+          title: "Current Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
